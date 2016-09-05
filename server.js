523b9ddc8f32185b8267feaab9f1e739d88735b7
SHALINI RAWAT <SHALINI RAWAT>
2016-09-05 19:27:56 +0530

    morgan added to lock req in server

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 //for using express it has all the methods like get ,post, put, delete
 var express = require('express');
 
+//to show the req by the uer in server
+var morgan = require('morgan');
+
 //app is referring to express objects
 var app = express();
+
+//middlEWARE
+app.use(morgan('dev'));
 
 //it tells the server to response something, get method 
 app.get('/name', function(req,res){
@@ -12,6 +18,8 @@ app.get('/name', function(req,res){
 //sending something to server, post method
 /*app.post()-to send data to server,app.delete(), app.put()-updating data
 */
+
+
 app.listen(3000, function(err){
 	if(err) throw err;
 	console.log("Server is running");
